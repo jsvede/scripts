29de1c7a75cd77e3183f5b04a0149d0987af17c7
Jon Svede <jsvede@yahoo.com>
2014-03-10 18:28:32 -0600

    Added code to properly set the password and send an e-mail to the user.

--- uadd.sh
+++ uadd.sh
@@ -8,16 +8,44 @@ genpasswd() {
         tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
 }
 
-
 if [ $# != 1 ];
 then
-    echo Please supply a username.
+    echo Please supply a username. An e-mail address optional.
+    echo uadd.sh someone someone@charter.com
     exit
 fi
 
-defaultPasswd=$(genpasswd 8)
+EMAIL=$2
 
-sudo useradd $1 -p $defaultPasswd
+genPasswd=$(genpasswd 8)
+
+sudo useradd $1
+echo $genPasswd | passwd $1 --stdin
 sudo chage -d 0 $1
 
-echo new user $1 created with default password $defaultPasswd
+MESSAGE="/tmp/uaddmailmsg"
+
+if [ -e $MESSAGE ];
+then
+    rm -rf $MESSAGE
+fi
+
+HOSTNAME=$(hostname)
+IPADDR=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+echo "On hostname $HOSTNAME with IP address $IPADDR there is a new user $1 created with default password $genPasswd"> $MESSAGE
+
+if [ "$2" != "" ];
+then
+    echo "Sending e-mail to $2"
+    SUBJECT="New user account on $HOSTNAME"
+    /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE
+    echo "Message sent."
+fi
+
+cat $MESSAGE
+
+if [ -e $MESSAGE ];
+then
+    rm -rf $MESSAGE
+fi
+~ 
